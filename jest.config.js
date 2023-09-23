9db777fc3133cf91d2e63b63ef01c18ad84e7f8a
Jeff <doylejeff98@gmail.com>
2023-09-23 01:46:09 -0400

    sum and test passes

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,9 @@ module.exports = {
     moduleDirectories: ['node_modules', 'bower_components', 'shared'],
 
     moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
         '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-        '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-
-        '^react(.*)$': '<rootDir>/vendor/react-master$1',
         '^config$': '<rootDir>/configs/app-config.js',
     },
 };
